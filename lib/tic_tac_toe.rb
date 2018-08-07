4a98553bdb55210f859aeac4ce33d5c74b1fc9dc
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-07 05:37:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ class TicTacToe
   end
   
   def position_taken?(index)
-    @board[location] != " " && @board[location] != "" 
+    @board[index] != " " && @board[index] != "" 
   end
   
   def move(index, current_player)
